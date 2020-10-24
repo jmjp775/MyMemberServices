a4c1e6fd66fc2ff1c465f24f0531bed2ba2492a7
jmjp775 <jmjp775@gmail.com>
2020-10-24 12:38:46 -0400

    still wip, need to add pages/razor pages using ER

--- Models/ServiceAccountRequest.cs
+++ Models/ServiceAccountRequest.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyMemberServices.Models
 {
     public class ServiceAccountRequest
     {
+        public int RequestID { get; set; }
+        public string ServiceAccountName { get; set; }
+        public string NonProdOrProdRequest { get; set; }
+        public string RequestStatus { get; set; }
+        public string ClientAppName { get; set; }
+        public string ContactName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string ContactEmail { get; set; }
+        public string ContactPhoneNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
 
     }
 }
